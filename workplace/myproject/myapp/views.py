4841dfb82ce258dc0fe3c76c0049e35c27b0f9ed
Danish2331 <danishdj2331dj@gmail.com>
2025-03-04 22:12:09 +0530

    Changes max2

---
+++ workplace/myproject/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Little Lemon!")
+
+# Create your views here for menu.
+
+# Create view for About
+
+# Create view for Menu
